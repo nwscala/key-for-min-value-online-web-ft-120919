8caaf97220e0c5e6b270cb8d0e4b33ca8d6cbefe
nwscala <nwscala@gmail.com>
2019-12-12 16:16:33 -0500

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,16 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+    min_value = Float::INFINITY 
+    min_name = ""
+    name_hash.each do |name, number|
+        if number < min_value
+            min_value = number
+            min_name = name
+        end
+    end
+    if min_value == Float::INFINITY
+        return nil
+    end
+    min_name
 end
